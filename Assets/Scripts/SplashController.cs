220c3febb6a5aa2d1f4d7ef50129a39fb1a4e2cb
Jeremy Howa <jrhowa@inmotionsoftware.com>
2015-06-20 23:19:52 -0500

    - Added timer to the splash screen and added it to the first entry in the app settings. -

---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashController : MonoBehaviour 
+{
+	void Start()
+	{
+		StartCoroutine(WaitAndContinue(5.0F));
+	}
+
+	IEnumerator WaitAndContinue(float waitTime) 
+	{
+		// Wait 
+		yield return new WaitForSeconds(waitTime);
+
+		// Load level
+		Application.LoadLevel("MainMenu");
+	}
+}
