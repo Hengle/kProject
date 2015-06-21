c508e7e93413a8b315d6b10085f34ffb3f3f23ca
Jeremy Howa <jrhowa@inmotionsoftware.com>
2015-06-21 00:42:00 -0500

    Added sound to splash screen.

--- Assets/Scripts/SplashController.cs
+++ Assets/Scripts/SplashController.cs
@@ -1,11 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SplashController : MonoBehaviour 
 {
+	public Image m_TitleImage;
+	float m_Alpha = 0.0f;
 	void Start()
 	{
-		StartCoroutine(WaitAndContinue(5.0F));
+		StartCoroutine(WaitAndContinue(5.5f));
+		Color col = m_TitleImage.color;
+		col.a = 0;
+		m_TitleImage.color = col;
+	}
+
+	void Update()
+	{
+		Color col = m_TitleImage.color;
+		m_Alpha += (0.0009f) * Time.deltaTime;
+		col.a = 255.0f * m_Alpha;
+		m_TitleImage.color = col;
+
+
+
 	}
 
 	IEnumerator WaitAndContinue(float waitTime) 
